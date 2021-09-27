47bf9bbd48a86fd5228b084fa6ec7f076f0e09df
webgtx <dxv1d@tuta.io>
2021-09-27 03:14:58 +0300

    Added config & writed setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "Setup script started"
 
+if [[ $1 == '--deb' ]] || [[ $1 == '' ]]; then
+  apt list --installed | grep "nvim"
+elif [[ $1 == '--arch' ]]; then
+  sudo pacman -Sy neovim
+fi
+cp -r nvim $HOME/.config/
