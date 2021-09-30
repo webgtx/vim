b19b67fbcff68ff9e0590041bbcd669e6a8c0f79
webgtx <dxv1d@tuta.io>
2021-10-01 01:56:17 +0300

    added new arugment (--without)

--- setup.sh
+++ setup.sh
@@ -6,5 +6,7 @@ if [[ $1 == '--deb' ]] || [[ $1 == '' ]]; then
   apt list --installed | grep "nvim"
 elif [[ $1 == '--arch' ]]; then
   sudo pacman -Sy neovim
+elif [[ $1 == '--without' ]]; then
+  echo "Fine"
 fi
 cp -r nvim $HOME/.config/
