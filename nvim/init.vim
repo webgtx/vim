43d1aa5aceb1a966a413428dbe2f8936a45e26d7
Alex Zolotarov <dxv1d@tuta.io>
2021-10-10 17:44:47 +0300

    added vim-plug, & emmit

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,1 +1,7 @@
 source $HOME/.config/nvim/general/settings.vim
+
+call plug#begin('~/.vim/plugged')
+
+Plug 'mattn/emmet-vim'
+
+call plug#end()
--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 
 echo "Setup script started"
 
-if [[ $1 == '--deb' ]] || [[ $1 == '' ]]; then
+if [[ $1 == '--deb' ]]; then
   apt list --installed | grep "nvim"
 elif [[ $1 == '--arch' ]]; then
   sudo pacman -Sy neovim
-elif [[ $1 == '--without' ]]; then
-  echo "Fine"
+elif [[ $1 == '--simple' ]]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+elif [[ $1 == '' ]]; then
+  echo "Okay, starting installtion without testing..."
 fi
+
 cp -r nvim $HOME/.config/
+echo Done!
+
