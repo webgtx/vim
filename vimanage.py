f5fee56c86cd4ab49062f6fa653322e7cd595dac
webgtx <alx.zolotarov@gmail.com>
2023-11-25 21:17:41 +0000

    Update: formmating with black

--- vimanage.py
+++ vimanage.py
@@ -3,13 +3,14 @@ import subprocess
 from os import environ, remove, listdir
 from shutil import copytree, copyfile, rmtree
 
-class VIManager():
+
+class VIManager:
     def __init__(self):
         self.plugins_path = f"{environ['HOME']}/.vim/pack/plugins/start"
-        self.home_path = environ['HOME']
+        self.home_path = environ["HOME"]
         self.intergrations = {
             "terraform": f"git clone https://github.com/hashivim/vim-terraform.git {self.plugins_path}/vim-terraform",
-            "d2": f"git clone https://github.com/terrastruct/d2-vim {self.plugins_path}/d2-vim"
+            "d2": f"git clone https://github.com/terrastruct/d2-vim {self.plugins_path}/d2-vim",
         }
 
     def install(self):
@@ -28,18 +29,20 @@ class VIManager():
         self,
         name: str = "industry",
         termlinebg: tuple = ("25", "240"),
-        show: bool = False
+        show: bool = False,
     ):
         if show:
             theme_registry = listdir(".vim/colors")
             return "Available colorschemes:\n" + "\n".join(theme_registry)
 
         with open(f"{self.home_path}/.vim/general/theme.vim", "w") as themeconf:
-            themeconf.write((
-                f"colorscheme {name}\n"
-                f"hi StatusLineTerm ctermbg={termlinebg[0]} ctermfg=255\n"
-                f"hi StatusLineTermNC ctermbg={termlinebg[1]} ctermfg=255"
-            ))
+            themeconf.write(
+                (
+                    f"colorscheme {name}\n"
+                    f"hi StatusLineTerm ctermbg={termlinebg[0]} ctermfg=255\n"
+                    f"hi StatusLineTermNC ctermbg={termlinebg[1]} ctermfg=255"
+                )
+            )
 
         return f"Colorscheme: {name}\nTerminalLineBG: {termlinebg}"
 
@@ -51,7 +54,5 @@ class VIManager():
         return f"Installed intergrations: {list(plugins.keys())}"
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(VIManager)
-
--- vippets.py
+++ vippets.py
@@ -6,9 +6,11 @@ with open(argv[1], "r") as stream:
     snippets = yaml.safe_load(stream)
     try:
         remove(f"{pwd()}/{argv[2]}")
-    except FileNotFoundError: 
+    except FileNotFoundError:
         print("\x1b[31mFile wasn't found\033[0m")
     for snippet in snippets:
-        snippet["code"] = f"ab {snippet['name']} " + snippet["code"].replace("\n", "<CR>") + "\n"
+        snippet["code"] = (
+            f"ab {snippet['name']} " + snippet["code"].replace("\n", "<CR>") + "\n"
+        )
         with open(argv[2], "a") as output_stream:
             output_stream.write(snippet["code"])
