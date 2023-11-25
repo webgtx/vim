e4a65deaeee2d1b4bda500da59a68222bcae0500
webgtx <alx.zolotarov@gmail.com>
2023-11-25 22:34:23 +0000

    Fix: vippets indent issue fixed

--- vippets.py
+++ vippets.py
@@ -4,13 +4,12 @@ from os import remove, path, getcwd as pwd
 
 with open(argv[1], "r") as stream:
     snippets = yaml.safe_load(stream)
+    snippets_binary = str()
     try:
         remove(f"{pwd()}/{argv[2]}")
     except FileNotFoundError:
         print("\x1b[31mFile wasn't found\033[0m")
     for snippet in snippets:
-        snippet["code"] = (
-            f"ab {snippet['name']} " + snippet["code"].replace("\n", "<CR>") + "\n"
-        )
-        with open(argv[2], "a") as output_stream:
-            output_stream.write(snippet["code"])
+        snippets_binary += (f"ab {snippet['name']} " + snippet["code"].replace("\n", f"<CR><Home>") + "\n")
+    with open(argv[2], "w") as output_stream:
+        output_stream.write(snippets_binary)
